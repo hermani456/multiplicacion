7b58b107fb0614bb4341ad46e47d06508e92f55a
hermani456 <hermani457@gmail.com>
2022-02-17 19:31:32 -0300

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 // funcion que se asegura que el numero este dentro del rango
-const comparacion = (n) => n < 1 || n > 20;
+const comparacion = n => n < 1 || n > 20;
 
 // funcion que pide un numero entre el 1 y el 20 y lo retorna
-const numUsuario = (comp) => {
-   const numero = prompt("Ingresa un numero del 1 al 20");
+const numUsuario = comp => {
+   const numero = parseInt(prompt("Ingresa un numero del 1 al 20"));
    do {
-      numero
       if (comp(numero)) {
          alert("Numero fuera de rango");
       }
@@ -22,7 +21,7 @@ for (i = 1; i <= numeroIngresado; i++) {
 }
 
 // funcion que retorna el factorial de un numero ingresado
-const factorialNumero = (x) => {
+const factorialNumero = x => {
    if (x > 1) {
       return x * factorialNumero(x - 1);
    } else {
