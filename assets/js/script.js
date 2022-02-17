02c9798fea9a81fbd07b5ef1f1aab7d454099d5d
hermani456 <hermani457@gmail.com>
2022-02-17 00:53:47 -0300

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
+// funcion que se asegura que el numero este dentro del rango
 const comparacion = (n) => n < 1 || n > 20;
 
-const numUsuario = () => {
+// funcion que pide un numero entre el 1 y el 20 y lo retorna
+const numUsuario = (comp) => {
    do {
       var numero = prompt("Ingresa un numero del 1 al 20");
-      if (comparacion(numero)) {
+      if (comp(numero)) {
          alert("Numero fuera de rango");
       }
-   } while (comparacion(numero));
+   } while (comp(numero));
    return numero;
 };
 
-const numeroIngresado = numUsuario();
+// constante que guarda el numero ingresado
+const numeroIngresado = numUsuario(comparacion);
 
+// loop que multiplica desde 1 hasta el numero ingresado por el usuario
 for (i = 1; i <= numeroIngresado; i++) {
    document.write(`${i} x ${numeroIngresado} = ${i * numeroIngresado}<br>`);
 }
 
+// funcion que retorna el factorial de un numero ingresado
 const factorialNumero = (x) => {
    if (x > 1) {
       return x * factorialNumero(x - 1);
@@ -24,6 +29,7 @@ const factorialNumero = (x) => {
    }
 };
 
+// loop que muestra el factorial desde 1 hasta el numero ingresado por el usuario
 for (i = 1; i <= numeroIngresado; i++) {
    document.write(`El factorial de ${i} es ${factorialNumero(i)}<br>`);
 }
